b46a2b158817b30cda2ebf04a7cdc1a53a175677
Mason Raasch <raasch.mason@gmail.com>
2018-11-10 15:02:11 -0800

    get a checksum

--- verifier.cpp
+++ verifier.cpp
@@ -1,7 +1,7 @@
-#include <eosiolib/eosio.hpp>
-#include <eosiolib/print.hpp>
-#include <eosiolib/types.h>
-#include <eosiolib/crypto.h>
+#include "eosiolib/eosio.hpp"
+#include "eosiolib/print.hpp"
+#include "eosiolib/types.h"
+#include "eosiolib/crypto.h"
 
 using namespace eosio;
 
@@ -11,6 +11,11 @@ class verifier : public contract {
 
       [[eosio::action]]
       void assrtrcvrky() {
+        capi_checksum256 result;
+        const char* myString = "yo waddup";
+        // below generates and unpacks a valid checksum256 
+        sha256(myString, sizeof(myString), &result);
       }
+
 };
 EOSIO_DISPATCH(verifier, (assrtrcvrky))
