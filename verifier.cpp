1ad1cdf82169be390ee50dda368a7336d0e9aa12
Mason Raasch <raasch.mason@gmail.com>
2018-11-10 15:56:09 -0800

    getting the multi index defined

--- verifier.cpp
+++ verifier.cpp
@@ -10,12 +10,25 @@ class verifier : public contract {
       using contract::contract;
 
       [[eosio::action]]
-      void assrtrcvrky() {
+      void assrtrcvrky(const capi_checksum256 digest, const char sig, size_t siglen, const char pub, size_t publen ) {
         capi_checksum256 result;
         const char* myString = "yo waddup";
         // below generates and unpacks a valid checksum256 
         sha256(myString, sizeof(myString), &result);
       }
+
+  private:
+    struct [[eosio::table]] pkeytoutxo {
+      uint64_t key;
+      std::string publicKey;
+      capi_checksum256 signature;
+      uint64_t primary_key() const { return key; }
+
+      std::string get_by_public_key() const { return publicKey; }
+
+    };
+    typedef eosio::multi_index<"pkeytoutxo"_n, pkeytoutxo,
+      eosio::indexed_by< "publicKey"_n, eosio::const_mem_fun<pkeytoutxo, std::string, &pkeytoutxo::get_by_public_key>>> txtable;
 
 };
 EOSIO_DISPATCH(verifier, (assrtrcvrky))
