fc23790275d1f04c27aa9fec03236c84ce6ff38b
Chris Atanasian <catanasian@gmail.com>
2018-11-10 16:29:53 -0800

    Fix table

--- verifier.cpp
+++ verifier.cpp
@@ -4,35 +4,42 @@
 #include "eosiolib/crypto.h"
 
 using namespace eosio;
+using namespace std;
 
 class verifier : public contract {
   public:
       using contract::contract;
 
       [[eosio::action]]
-      void assrtrcvrky(const capi_checksum256 digest, const char sig, size_t siglen, const char pub, size_t publen ) {
+      void assrtrcvrky(const capi_checksum256 digest,
+                       const char sig,
+                       size_t siglen,
+                       const char pub,
+                       size_t publen) {
         capi_checksum256 result;
         const char* myString = "yo waddup";
         // below generates and unpacks a valid checksum256
         sha256(myString, sizeof(myString), &result);
+
+        txtable transactions(_self, _self.value);
       }
 
   private:
     struct [[eosio::table]] pkeytoutxo {
       uint64_t key;
-      std::string publicKeyFrom;
-      std::string publicKeyTo;
-      float amount;
+      string publicKeyFrom;
+      string publicKeyTo;
+      double amount;
       capi_checksum256 signature;
       uint64_t primary_key() const { return key; }
 
-      std::string get_by_public_key_from() const { return publicKeyTo; }
-      std::string get_by_public_key_to() const { return publicKeyFrom; }
+      string publickeyfrom() const { return publicKeyFrom; }
+      string publickeyto() const { return publicKeyTo; }
     };
 
-    typedef eosio::multi_index<name("pkeytoutxo"), pkeytoutxo,
-      indexed_by<name("pblckyfrm"), const_mem_fun<pkeytoutxo, std::string, &pkeytoutxo::get_by_public_key_from>>,
-      indexed_by<name("pblckyto"), const_mem_fun<pkeytoutxo, std::string, &pkeytoutxo::get_by_public_key_to>>
+    typedef eosio::multi_index<"pkeytoutxo"_n, pkeytoutxo,
+      indexed_by<"pblckyfrm"_n, const_mem_fun<pkeytoutxo, string, &pkeytoutxo::publickeyfrom>>,
+      indexed_by<"pblckyto"_n, const_mem_fun<pkeytoutxo, string, &pkeytoutxo::publickeyto>>
     > txtable;
 
 };
