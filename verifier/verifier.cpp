3cbf5f80db4c5db22c9558eb0576c50ec4483265
kedar <kedarmail@gmail.com>
2018-11-26 15:10:38 -0800

    verify only

--- verifier/verifier.cpp
+++ verifier/verifier.cpp
@@ -12,20 +12,21 @@ class verifier : public contract {
       using contract::contract;
 
       [[eosio::action]]
-      void transfer(public_key pkeyfrom,
-                    public_key pkeyto,
+      void transfer(public_key pkeyFrom,
+                    public_key pkeyTo,
                     asset amount,
                     asset fee,
                     signature sig,
                     string memo) {
-        //require_auth(_self);
-        //eosio_assert(string(pkeyFrom.data) != string(pkeyTo.data), "cannot transfer to self");
+        require_auth(_self);
+        eosio_assert(pkeyFrom != pkeyTo, "cannot transfer to self");
 
         std::string data = "test_data";
-        checksum256 digest = sha256(data.c_str(), data.length());
-        assert_recover_key(digest, sig, pkeyfrom);
-      }
+        checksum256 digest = sha256(data, sizeof(data), &digest);
 
+        assert_recover_key(digest, sig, pkeyFrom);
+
+      }
 };
 
 EOSIO_DISPATCH(verifier, (transfer))
